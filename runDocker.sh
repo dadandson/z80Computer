baf63e5f50f91ebb96faca4c828e01da83235539
cmdc0de <cmdc0dez@gmail.com>
2020-01-17 22:13:43 -0700

    added fuse

--- runDocker.sh
+++ runDocker.sh
@@ -3,4 +3,4 @@ set -e
 
 ESPCONTAINERVERSION=`cat docker/espcontainerver`
 
-docker run --name z80 --rm -it --privileged -e USER_ID=$UID -e GROUP_ID=`id -g` -v /dev/bus/usb:/dev/bus/usb -v $PWD:/home/z80/project z80:$ESPCONTAINERVERSION 
+docker run --name z80 --rm -it --privileged -e USER_ID=$UID -e GROUP_ID=`id -g` -e DISPLAY=$DISPLAY -v /dev/bus/usb:/dev/bus/usb -v $PWD:/home/z80/project -v /tmp/.X11-unix:/tmp/.X11-unix z80:$ESPCONTAINERVERSION 
---
+++ start_fuse.sh
@@ -0,0 +1,2 @@
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
+fuse -g 3x
